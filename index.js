583366162e47316e78c4c7a2c8ce01d231be6dbd
Juan Galvis <juangalvis.sh@gmail.com>
2022-06-04 15:55:14 -0500

    Fetch date Google sheet

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
 const CORS = require('cors');
 
 app.use(CORS());
 
-app.get('/image', (req, res) => {
-    console.log(req.headers);
+app.get('/image', async (req, res) => {
+    const response = await fetch("https://sheetdb.io/api/v1/391a4kw4e1zbh", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"data": [{"Fecha": new Date().toLocaleString('es-ES', {
+            timeZone: 'America/Bogota'
+        })}]})
+    })
+    console.log(response);
     res.sendFile(__dirname+'/image.png');
 })
 
-app.listen('3000', () => {
-    console.log("Servidor corriendo en el puerto 3000");
+app.listen(process.env.PORT, () => {
+    console.log("Servidor corriendo en el puerto "+process.env.PORT);
 })
